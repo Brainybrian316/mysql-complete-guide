0fd4f553f09fed9427379e9ce79818f6a2114499
Brainybrian316 <brainybrian316@gmail.com>
2022-10-07 13:39:36 -0400

    employer table

---
+++ 05-create-employeres-table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE employers (
+  company_name VARCHAR(250),
+  company_address VARCHAR(300),
+  -- Aproximation approach left is digit amount right is after the . 
+  -- yearly_revenue FLOAT(5, 2)
+  -- exact approach
+  yearly_revenue NUMERIC(5, 2),
+  is_hiring BOOLEAN 
+  );
