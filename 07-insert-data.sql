8675c290f64a17f8cd2a1b5b8e67c533fbf1b45b
Brainybrian316 <brainybrian316@gmail.com>
2022-10-07 13:58:42 -0400

    insertted data into tables

---
+++ 07-insert-data.sql
@@ -0,0 +1,31 @@
+-- INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring) 
+-- VALUES (
+--   'Learning Inc',
+--   'Educationstreet 12, London',
+--   0.87,
+--   true 
+-- );
+
+-- INSERT INTO employers(company_name, company_address, yearly_revenue, is_hiring) 
+-- VALUES(
+--   'Big oil Inc',
+--   'Slipperystreet 110, Houston',
+--   112.55,
+--   false 
+-- );
+
+-- INSERT INTO employers(company_name, company_address, yearly_revenue, is_hiring) 
+-- VALUES(
+--   'Hipster Food Inc',
+--   'Avocadostreet 5, Berlin',
+--   6.12,
+--   true 
+-- );
+
+INSERT INTO conversations (user_name, employer_name, messages, date_sent)
+VALUES (
+  'Max Schwarz', 
+  'Learning Inc', 
+  'Hi, I like Learning',
+   '2022-10-7 16:30:10'
+);
