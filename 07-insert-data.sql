25c32649136c117395682b3bfdeeaeaaca7dd267
Brainybrian316 <brainybrian316@gmail.com>
2022-10-07 14:11:35 -0400

    drop table and re add

--- 07-insert-data.sql
+++ 07-insert-data.sql
@@ -1,31 +1,29 @@
--- INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring) 
--- VALUES (
---   'Learning Inc',
---   'Educationstreet 12, London',
---   0.87,
---   true 
--- );
+INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring) 
+VALUES (
+  'Learning Inc',
+  'Educationstreet 12, London',
+  0.87,
+  true 
+);
 
--- INSERT INTO employers(company_name, company_address, yearly_revenue, is_hiring) 
--- VALUES(
---   'Big oil Inc',
---   'Slipperystreet 110, Houston',
---   112.55,
---   false 
--- );
+INSERT INTO employers (company_name, company_address, yearly_revenue) 
+VALUES (
+  'Big oil Inc',
+  'Slipperystreet 110, Houston',
+  112.55
+);
 
--- INSERT INTO employers(company_name, company_address, yearly_revenue, is_hiring) 
--- VALUES(
---   'Hipster Food Inc',
---   'Avocadostreet 5, Berlin',
---   6.12,
---   true 
--- );
+INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring) 
+VALUES (
+  'Hipster Food Inc',
+  'Avocadostreet 5, Berlin',
+  6.12,
+  true 
+);
 
-INSERT INTO conversations (user_name, employer_name, messages, date_sent)
+INSERT INTO conversations (user_name, employer_name, messages)
 VALUES (
   'Max Schwarz', 
   'Learning Inc', 
-  'Hi, I like Learning',
-   '2022-10-7 16:30:10'
+  'Hi, I like Learning'
 );
