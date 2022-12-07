6acf676a78a4b8fd7ab8072f378e3af1c5b00952
Brainybrian316 <brainybrian316@gmail.com>
2022-12-07 16:23:48 -0500

    basic filtering

--- 07-select-data.sql
+++ 07-select-data.sql
@@ -4,3 +4,19 @@ customer_name,
 product_name,
 volume
 FROM sales;
+
+-- -- !alias
+-- SELECT 
+-- date_created,
+-- customer_name,
+-- product_name,
+-- volume AS total_sales
+-- FROM sales;
+
+-- -- !expressions
+-- SELECT 
+-- date_created,
+-- customer_name,
+-- product_name,
+-- volume / 100 AS total_sales
+-- FROM sales;
