795e71da28a409e4ec3a8be9f5af978e51649d7f
Brainybrian316 <brainybrian316@gmail.com>
2022-12-13 16:06:27 -0500

    filter with text

---
+++ 10-filter-with-text.sql
@@ -0,0 +1,7 @@
+--  *inequality
+-- SELECT * FROM sales
+-- WHERE customer_name <> 'Max Schwarz';
+
+-- *compareds letter to leter starting with the first one
+-- SELECT * FROM sales
+-- WHERE customer_name < 'Company A';
