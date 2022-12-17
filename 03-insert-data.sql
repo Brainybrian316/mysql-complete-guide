004b84222d5dba9cea125b3b27184c91ec484749
Brainybrian316 <brainybrian316@gmail.com>
2022-12-17 18:46:53 -0500

    insert data

---
+++ 03-insert-data.sql
@@ -0,0 +1,10 @@
+-- INSERT INTO cities (name)
+-- VALUES ('Berlin'), ('New York'), ('London');
+
+-- INSERT INTO addresses (street, house_number, city_id)
+-- VALUES 
+-- ('Teststreet', '8A', 3),
+--  ('Some Street', '10', 1),
+--   ('Teststreet', '1', 3),
+--    ('My street', '101', 2);
+
