3dcb2ec25ab533e65bef639707a4f24ebe9216e5
Brainybrian316 <brainybrian316@gmail.com>
2022-12-20 16:03:14 -0500

    inner join

---
+++ 04-inner-join.sql
@@ -0,0 +1,8 @@
+-- SELECT * 
+-- FROM users AS u
+-- INNER JOIN addresses AS a ON u.address_id = a.id;
+
+--  *fine tuning for specific columns
+SELECT u.id, first_name, last_name, street, house_number, city_id
+ FROM users AS u
+INNER JOIN addresses AS a ON u.address_id = a.id;
