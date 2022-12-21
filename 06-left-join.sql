1ce31fa1570a27d7dd37c9ea7926b574e120f781
Brainybrian316 <brainybrian316@gmail.com>
2022-12-21 15:49:52 -0500

    right join

--- 06-left-join.sql
+++ 06-left-join.sql
@@ -4,7 +4,13 @@
 -- LEFT JOIN addresses AS a ON a.id = u.address_id;
 
 --  *will get all address data and only users that have a relation/match with the address table
+-- SELECT *
+-- FROM addresses AS a
+-- LEFT JOIN users AS u ON a.id = u.address_id
+-- LEFT JOIN cities AS c ON c.id = a.city_id;
+
+--  *right and left jolns are same except the values after from...left join is common practice
 SELECT *
-FROM addresses AS a
-LEFT JOIN users AS u ON a.id = u.address_id
+FROM users AS a
+RIGHT JOIN addresses AS a ON a.id = u.address_id
 LEFT JOIN cities AS c ON c.id = a.city_id;
