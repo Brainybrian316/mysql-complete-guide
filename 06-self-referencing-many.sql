38af41d8212607752b85dafb288b19afe56a06ad
Brainybrian316 <brainybrian316@gmail.com>
2023-01-03 12:52:39 -0500

    self referencing many to many

--- 06-self-referencing-many.sql
+++ 06-self-referencing-many.sql
@@ -1,14 +1,28 @@
---  *postgres
+-- --  *postgres
+-- CREATE TABLE users (
+--   id SERIAL PRIMARY KEY,
+--   first_name VARCHAR(300) NOT NULL
+-- );
+-- --  *postgres
+-- CREATE TABLE users_friends (
+--   user_id INT REFERENCES users ON DELETE CASCADE,
+--   friend_id INT REFERENCES users ON DELETE CASCADE,
+--   -- This ensures that there won't be duplicates and that user_id is always less than the friends id EX: 1 & 2... we wont see a 2 & 1 as its the same with this check
+--   CHECK (user_id < friend_id),
+--   --  this is a composite key
+--   PRIMARY KEY (user_id, friend_id)
+-- );
+--  !mysql
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   first_name VARCHAR(300) NOT NULL
 );
---  *postgres
+--  !mysql
 CREATE TABLE users_friends (
-  user_id INT REFERENCES users ON DELETE CASCADE,
-  friend_id INT REFERENCES users ON DELETE CASCADE,
-  -- This ensures that there won't be duplicates and that user_id is always less than the friends id EX: 1 & 2... we wont see a 2 & 1 as its the same with this check
+  user_id INT,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+  friend_id INT,
+  FOREIGN KEY (friend_id) REFERENCES users (id) ON DELETE CASCADE,
   CHECK (user_id < friend_id),
-  --  this is a composite key
   PRIMARY KEY (user_id, friend_id)
 );
--- 07-insert-many-to-many-data.sql
+++ 07-insert-many-to-many-data.sql
@@ -1,10 +1,10 @@
--- -- statement 1
+-- -- *statement 1
 -- INSERT INTO users (first_name)
 -- VALUES ('Manuel'),
 --   ('Max'),
 --   ('Julie');
--- statement 2
-INSERT INTO users_friends (user_id, friend_id)
-VALUES (1, 2),
-  (1, 3),
-  (2, 3);
+-- -- *statement 2
+-- INSERT INTO users_friends (user_id, friend_id)
+-- VALUES (1, 2),
+--   (1, 3),
+--   (2, 3);
