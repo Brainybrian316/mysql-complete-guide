ba36b4c1efa3cd0cdca251192728e47b8ee7708c
Brainybrian316 <brainybrian316@gmail.com>
2023-01-06 14:41:19 -0500

    gym data

--- 02-create-table.sql
+++ 02-create-table.sql
@@ -1,28 +1,28 @@
--- !mysql
-CREATE TABLE memberships (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  membership_start DATE,
-  membership_end DATE,
-  last_checkin TIMESTAMP,
-  last_checkout TIMESTAMP,
-  consumption NUMERIC(5, 2),
-  first_name VARCHAR(200),
-  last_name VARCHAR(200),
-  price NUMERIC(5, 2),
-  billing_frequency INT,
-  gender VARCHAR(200)
-);
--- *postgres
-CREATE TABLE memberships (
-  id SERIAL PRIMARY KEY,
-  membership_start DATE,
-  membership_end DATE,
-  last_checkin TIMESTAMP,
-  last_checkout TIMESTAMP,
-  consumption NUMERIC(5, 2),
-  first_name VARCHAR(200),
-  last_name VARCHAR(200),
-  price NUMERIC(5, 2),
-  billing_frequency INT,
-  gender VARCHAR(200)
-);
+-- -- !mysql
+-- CREATE TABLE memberships (
+--   id INT PRIMARY KEY AUTO_INCREMENT,
+--   membership_start DATE,
+--   membership_end DATE,
+--   last_checkin TIMESTAMP,
+--   last_checkout TIMESTAMP,
+--   consumption NUMERIC(5, 2),
+--   first_name VARCHAR(200),
+--   last_name VARCHAR(200),
+--   price NUMERIC(5, 2),
+--   billing_frequency INT,
+--   gender VARCHAR(200)
+-- );
+-- -- *postgres
+-- CREATE TABLE memberships (
+--   id SERIAL PRIMARY KEY,
+--   membership_start DATE,
+--   membership_end DATE,
+--   last_checkin TIMESTAMP,
+--   last_checkout TIMESTAMP,
+--   consumption NUMERIC(5, 2),
+--   first_name VARCHAR(200),
+--   last_name VARCHAR(200),
+--   price NUMERIC(5, 2),
+--   billing_frequency INT,
+--   gender VARCHAR(200)
+-- );
--- 04-create-additional-tables.sql
+++ 04-create-additional-tables.sql
@@ -1,27 +1,27 @@
--- !mysql
-CREATE TABLE customers(
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  first_name VARCHAR(200),
-  last_name VARCHAR(200),
-  email VARCHAR(200)
-);
--- !mysql
-CREATE TABLE orders(
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  amount_billed NUMERIC(5, 2),
-  customer_id INT,
-  FOREIGN KEY (customer_id) REFERENCES customers
-);
--- *postgres
-CREATE TABLE customers(
-  id SERIAL PRIMARY KEY,
-  first_name VARCHAR(200),
-  last_name VARCHAR(200),
-  email VARCHAR(200)
-);
--- *postgres
-CREATE TABLE orders(
-  id SERIAL PRIMARY KEY,
-  amount_billed NUMERIC(5, 2),
-  customer_id INT REFERENCES customers
-);
+-- -- !mysql
+-- CREATE TABLE customers(
+--   id INT PRIMARY KEY AUTO_INCREMENT,
+--   first_name VARCHAR(200),
+--   last_name VARCHAR(200),
+--   email VARCHAR(200)
+-- );
+-- -- !mysql
+-- CREATE TABLE orders(
+--   id INT PRIMARY KEY AUTO_INCREMENT,
+--   amount_billed NUMERIC(5, 2),
+--   customer_id INT,
+--   FOREIGN KEY (customer_id) REFERENCES customers (id)
+-- );
+-- -- *postgres
+-- CREATE TABLE customers(
+--   id SERIAL PRIMARY KEY,
+--   first_name VARCHAR(200),
+--   last_name VARCHAR(200),
+--   email VARCHAR(200)
+-- );
+-- -- *postgres
+-- CREATE TABLE orders(
+--   id SERIAL PRIMARY KEY,
+--   amount_billed NUMERIC(5, 2),
+--   customer_id INT REFERENCES customers
+-- );
